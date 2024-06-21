759b81ad3e2c9234ee5d0fe204fe8f5d47b54084
AllanAviana <allan.viana2003@gmail.com>
2024-06-21 03:06:25 -0300

    Add MovieData.kt to handle movie-related data structures and logic in the model package

---
+++ app/src/main/java/com/example/astroflix/model/MovieData.kt
@@ -0,0 +1,40 @@
+package com.example.astroflix.model
+
+data class MovieResponse(
+    val results: List<Movie>
+)
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    val overview: String,
+    val poster_path: String,
+    val backdrop_path: String?,
+    val release_date: String,
+    val vote_average: Float,
+    val vote_count: Int,
+    val genre_ids: List<Int>,
+    val original_language: String,
+    val original_title: String,
+    val popularity: Float,
+    val video: Boolean,
+    val adult: Boolean
+)
+
+data class WatchProvidersResponse(
+    val results: Map<String, CountryWatchProviders>
+)
+
+data class CountryWatchProviders(
+    val link: String,
+    val rent: List<Provider>?,
+    val buy: List<Provider>?,
+    val flatrate: List<Provider>?
+)
+
+data class Provider(
+    val logo_path: String,
+    val provider_id: Int,
+    val provider_name: String,
+    val display_priority: Int
+)
