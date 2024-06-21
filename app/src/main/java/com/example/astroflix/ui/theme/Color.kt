145a6aa9e8be8cb95d65e35de483a564d0050269
AllanAviana <allan.viana2003@gmail.com>
2024-06-21 02:08:34 -0300

    Initial setup of Android Studio project configuration

---
+++ app/src/main/java/com/example/astroflix/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.example.astroflix.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+val gray = Color(0xFF6E7A92)
+val darkGray = Color(0xFF21252C)
+val lightGray = Color(0xFF3A404B)
+val borderColor = Color(0xFF435070)
+val heartColor = Color(0xFF4A6781)
+val likeColor = Color(0xFFAB4242)
+
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
---
+++ app/src/main/java/com/example/astroflix/ui/theme/Type.kt
@@ -0,0 +1,46 @@
+package com.example.astroflix.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+
+
+
+
+val AstroFlixTypography = Typography(
+    bodyLarge = TextStyle(
+        fontSize = 32.sp,
+        color = Color.White,
+    ) ,
+    bodyMedium = TextStyle(
+        fontSize = 24.sp,
+        color = Color.White,
+    ),
+    labelMedium = TextStyle(
+        fontSize = 22.sp,
+        color = Color.White,
+    ),
+    bodySmall = TextStyle(
+        color = Color.White,
+        fontSize = 14.sp,
+        ),
+    titleMedium = TextStyle(
+        color = Color.White,
+        fontSize = 40.sp
+    ),
+    titleLarge = TextStyle(
+        color = Color.White,
+        fontSize = 64.sp
+    )
+
+
+
+
+
+
+)
