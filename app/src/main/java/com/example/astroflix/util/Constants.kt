9d8b6cb44f821a21fbf41fa94036ad122c968062
AllanAviana <allan.viana2003@gmail.com>
2024-08-03 15:00:57 -0300

    Add Constants

---
+++ app/src/main/java/com/example/astroflix/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.astroflix.util
+
+import androidx.compose.ui.unit.dp
+
+object Constants {
+   const val API_KEY = "API KEY"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    val PaddingSmall = 8.dp
+    val PaddingMedium = 16.dp
+
+}
